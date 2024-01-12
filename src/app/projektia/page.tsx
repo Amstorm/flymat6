40481b9f47878d34a9934c9650c47002601955f9
AMS <atis.meiers@gmail.com>
2024-01-12 08:51:28 +0200

    precizēts

--- src/app/projektia/page.tsx
+++ src/app/projektia/page.tsx
@@ -1,44 +1,29 @@
+import Post from "@/components/Post";
 import { cookieBasedClient, isAuthenticated } from "@/utils/amplify-utils";
-import { revalidatePath } from "next/cache";
-import { addComment, deleteComment } from "@/app/_actions/actions";
-import AddComment from "@/components/AddComment";
-import React from "react";
-import { Schema } from "../../../amplify/data/resource";
+import { onDeletePost } from "../_actions/actions";
 
-const Posts = async ({ params }: { params: { id: string } }) => {
-  if (!params.id) return null;
-
+export default async function Home() {
   const isSignedIn = await isAuthenticated();
-  const { data: post } = await cookieBasedClient.models.Post.get(
-    {
-      id: params.id,
-    },
-    {
-      authMode: "apiKey",
-      selectionSet: ["id", "title"],
-    }
-  );
-  const { data: allComments } = await cookieBasedClient.models.Comment.list({
+  const { data: posts } = await cookieBasedClient.models.Post.list({
+    selectionSet: ["title", "id"],
     authMode: "apiKey",
-    selectionSet: ["content", "post.id", "id"],
   });
 
-  const comments = allComments.filter(
-    (comment) => comment.post.id === params.id
-  );
+  console.log("posts", posts);
+
+  
 
   return (
     <main className="flex flex-col items-center justify-between p-24 w-1/2 m-auto">
-    <h1 className="text-2xl pb-10">Zemāk Matterport projekti admin lietotājiem pēc pieslēgšanās.</h1>
+      <h1 className="text-2xl pb-10">Zemāk integrējam, piemēram, publiski pieejamus Matterport projektus</h1>
 
-
-
+<iframe width='853' height='480' src='https://my.matterport.com/show/?m=mDTtRrrERwo'></iframe>
+{isSignedIn ? (
     <iframe width='853' height='480' src='https://my.matterport.com/show/?m=mDTtRrrERwo'></iframe>
 
+  ) : null}
 
-
-  </main>
+</main>
   );
-};
 
-export default Posts;
+}
